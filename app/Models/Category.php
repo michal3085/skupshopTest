a58be67d2275d0aee1cc7da0d03d7dbf13e99911
Michal Broszkiewicz <michal3085@gmail.com>
2025-05-23 10:06:09 +0200

    SHOP-task-seven

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Scopes\CategoryScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -14,4 +15,9 @@ class Category extends Model
         'slug',
         'popularity',
     ];
+
+    protected static function booted(): void
+    {
+        static::addGlobalScope('category', new CategoryScope);
+    }
 }
